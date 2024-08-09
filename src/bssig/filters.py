442551185f5eb05f1f5158d9f3a19ae5fc04dd8d
ng-23 <23grattan@gmail.com>
2024-08-08 20:30:00 -0400

    Minor changes

--- src/bssig/filters.py
+++ src/bssig/filters.py
@@ -3,17 +3,17 @@ Filters are implemented using Python and Blender compositor nodes
 See https://docs.blender.org/manual/en/latest/compositing/, specifically filter nodes and transform nodes
 """
 
-
 import scipy as sp
 import numpy as np
+from skimage import color
 
-def apply_gaussian_blur(img:np.array, sigma:float):
+def apply_gaussian_blur(img:np.ndarray, sigma:float):
     """Applies Gaussian blurring to the image
     """
 
     return sp.ndimage.gaussian_filter(img, sigma=sigma)
 
-def apply_gaussian_noise(img:np.array, variance:float, mean=0.0):
+def apply_gaussian_noise(img:np.ndarray, variance:float, mean=0.0):
     """Applies Gaussian noise to the image
     """
 
@@ -21,8 +21,8 @@ def apply_gaussian_noise(img:np.array, variance:float, mean=0.0):
 
     return np.clip(noisy_img, 0, 255).astype(np.uint8)
 
-def apply_grayscale():
-    pass
+def apply_grayscale(img:np.ndarray):
+    return color.rgb2gray(img)
 
 def apply_lens_flare():
     pass
